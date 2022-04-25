cf74d7d719c921e41825ed0e4e095a11479703bd
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-04-24 19:34:01 -0500

    Textos de forma condicional

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,33 +1,33 @@
-import Paciente from "./Paciente"
-
+import Paciente from "./Paciente";
 
 //props pacientes viene desde al app
-const ListadoPacientes = ( {pacientes} ) => {
+const ListadoPacientes = ({ pacientes }) => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">Administra tus {""}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
 
-      { pacientes.map( paciente =>(
-
-        // se pasa el prop {paciente hacia el componente paciente}
-       <Paciente 
-       key={paciente.id}
-        paciente={paciente}
-      />
-      ))}
-
-      
-    
-   
-
-
+          {pacientes.map((paciente) => (
+            // se pasa el prop {paciente hacia el componente paciente}
+            <Paciente key={paciente.id} paciente={paciente} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+          </p>
+        </>
+      )}
     </div>
-   
-  )
-}
+  );
+};
 
-export default ListadoPacientes
+export default ListadoPacientes;
